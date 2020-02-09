22d21df95e88718f33fd2d64f39b2915201d067b
Kwoth <master.kwoth@gmail.com>
2020-02-09 19:56:41 +0100

    merged small changes from global into 1.9

--- NadekoBot.Core/Services/Impl/StatsService.cs
+++ NadekoBot.Core/Services/Impl/StatsService.cs
@@ -24,7 +24,7 @@ namespace NadekoBot.Core.Services.Impl
         private readonly DateTime _started;
 
         public const string BotVersion = "2.29.13";
-        public string Author => "Kwoth#2560";
+        public string Author => "Kwoth#2452";
         public string Library => "Discord.Net";
 
         public string Heap => Math.Round((double)GC.GetTotalMemory(false) / 1.MiB(), 2)
