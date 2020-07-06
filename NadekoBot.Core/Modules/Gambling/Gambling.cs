06d36b4630d774b8e5587735ed8698f2e7a757d9
Kwoth <master.kwoth@gmail.com>
2020-07-06 12:57:08 +0200

    .lb should function properly again and not show pages. .lb --clean will show proper nbumber of pages and show users past first page. .lb page --clean will no longer always show the first page. closes #195

--- NadekoBot.Core/Modules/Gambling/Gambling.cs
+++ NadekoBot.Core/Modules/Gambling/Gambling.cs
@@ -487,13 +487,13 @@ namespace NadekoBot.Modules.Gambling
 
         [NadekoCommand, Usage, Description, Aliases]
         [NadekoOptions(typeof(LbOpts))]
-        [Priority(1)]
+        [Priority(0)]
         public Task Leaderboard(params string[] args)
             => Leaderboard(1, args);
 
         [NadekoCommand, Usage, Description, Aliases]
         [NadekoOptions(typeof(LbOpts))]
-        [Priority(0)]
+        [Priority(1)]
         public async Task Leaderboard(int page = 1, params string[] args)
         {
             if (--page < 0)
@@ -501,8 +501,7 @@ namespace NadekoBot.Modules.Gambling
 
             var (opts, _) = OptionsParser.ParseFrom(new LbOpts(), args);
 
-            List<DiscordUser> richest = new List<DiscordUser>();
-
+            List<DiscordUser> cleanRichest = new List<DiscordUser>();
             // it's pointless to have clean on dm context
             if (Context.Guild is null)
             {
@@ -515,45 +514,48 @@ namespace NadekoBot.Modules.Gambling
 
                 using (var uow = _db.GetDbContext())
                 {
-                    richest = uow.DiscordUsers.GetTopRichest(_client.CurrentUser.Id, 10_000);
+                    cleanRichest = uow.DiscordUsers.GetTopRichest(_client.CurrentUser.Id, 10_000);
                 }
                 var res = _tracker.LastDownloads.AddOrUpdate(Context.Guild.Id, now, (key, old) => (now - old) > TimeSpan.FromHours(1) ? now : old);
                 if (res == now)
                 {
+                    await Context.Channel.TriggerTypingAsync().ConfigureAwait(false);
                     await Context.Guild.DownloadUsersAsync().ConfigureAwait(false);
                 }
 
                 var sg = (SocketGuild)Context.Guild;
-                richest = richest.Where(x => sg.GetUser(x.UserId) != null)
+                cleanRichest = cleanRichest.Where(x => sg.GetUser(x.UserId) != null)
                     .ToList();
             }
             else
             {
                 using (var uow = _db.GetDbContext())
                 {
-                    richest = uow.DiscordUsers.GetTopRichest(_client.CurrentUser.Id, 9, page).ToList();
+                    cleanRichest = uow.DiscordUsers.GetTopRichest(_client.CurrentUser.Id, 9, page).ToList();
                 }
             }
 
             await Context.SendPaginatedConfirmAsync(page, curPage =>
             {
-                 var embed = new EmbedBuilder()
-                    .WithOkColor()
-                    .WithTitle(CurrencySign + " " + GetText("leaderboard"))
-                    .WithFooter(efb => efb.WithText(GetText("page", curPage)));
+                var embed = new EmbedBuilder()
+                   .WithOkColor()
+                   .WithTitle(CurrencySign + " " + GetText("leaderboard"));
 
-                if(!opts.Clean)
+                List<DiscordUser> toSend;
+                if (!opts.Clean)
                 {
                     using (var uow = _db.GetDbContext())
                     {
-                        richest = uow.DiscordUsers.GetTopRichest(_client.CurrentUser.Id, 9, curPage).ToList();
+                        toSend = uow.DiscordUsers.GetTopRichest(_client.CurrentUser.Id, 9, curPage);
                     }
                 }
-
-                 var toSend = richest.Skip(curPage * 9).Take(9).ToList();
+                else
+                {
+                    toSend = cleanRichest.Skip(curPage * 9).Take(9).ToList();
+                }
                  if (!toSend.Any())
                  {
-                     embed.WithDescription(GetText("no_users_found"));
+                     embed.WithDescription(GetText("no_user_on_this_page"));
                      return embed;
                  }
 
@@ -569,7 +571,7 @@ namespace NadekoBot.Modules.Gambling
                  }
 
                  return embed;
-             }, 10_000, 9);
+             }, opts.Clean ? cleanRichest.Count() : 9000, 9, opts.Clean);
         }
 
 
