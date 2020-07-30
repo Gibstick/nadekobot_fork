befb290da2cd45f855bcaee4e58722cbad21deae
Kwoth <master.kwoth@gmail.com>
2020-07-30 12:06:38 +0200

    release.ps1 update

--- release.ps1
+++ release.ps1
@@ -7,7 +7,7 @@ function Get-Changelog($lastTag)
 
     $tag = "$lastTag..HEAD"
 
-    $clArr = (& 'git' 'log', $tag, '--oneline')
+    $clArr = (git log $tag --oneline)
     [array]::Reverse($clArr)
     $changelog = $clArr | where { "$_" -notlike "*(POEditor.com)*" -and "$_" -notlike "*Merge branch*" -and "$_" -notlike "*Merge pull request*" -and "$_" -notlike "^-*" -and "$_" -notlike "*Merge remote tracking*" }
     $changelog = [string]::join([Environment]::NewLine, $changelog)
@@ -18,6 +18,8 @@ function Get-Changelog($lastTag)
         $cl2 = [string]::join([Environment]::NewLine, $cl2)
         $changelog = $changelog + "$nl ## Pull Requests Merged$nl$cl2"
     }
+
+    return $changelog
 }
 
 function Build-Installer($versionNumber)
@@ -31,6 +33,7 @@ function Build-Installer($versionNumber)
 
     & "iscc.exe" "/O+" ".\exe_builder.iss"
 
+    Write-ReleaseFile($versionNumber)
     # $path = [Environment]::GetFolderPath('MyDocuments') + "\_projekti\new_installer\$versionNumber\";
     # $binPath = $path + "nadeko-setup-$versionNumber.exe";
     # Copy-Item -Path $path -Destination $dest -Force -ErrorAction Stop
@@ -46,15 +49,15 @@ function Write-ReleaseFile($versionNumber) {
 	# $path = Build-Installer $versionNumber
 
 	# get changelog before tagging
-    # $changelog = Get-Changelog
+    $changelog = Get-Changelog
 	# tag the release
 	# & (git tag, $tag)
 
 	# print out the changelog to the console
     # Write-Host $changelog
 
-	$jsonReleaseFile = "[{""VersionName"": ""$versionNumber"", ""DownloadLink"": ""https://cdn.nadeko.bot/releases/nadeko-setup-$versionNumber.exe"", ""Changelog"": ""$changelog""}]"
+	$jsonReleaseFile = "[{""VersionName"": ""$versionNumber"", ""DownloadLink"": ""https://cdn.nadeko.bot/releases/nadeko-setup-$versionNumber.exe"", ""Changelog"": """"}]"
 
-	$releaseJsonOutPath = [Environment]::GetFolderPath('MyDocuments') + "\_projekti\NadekoInstallerOutput\$versionNumber\"
+	$releaseJsonOutPath = [Environment]::GetFolderPath('MyDocuments') + "\_projekti\nadeko-installers\$versionNumber\"
 	New-Item -Path $releaseJsonOutPath -Value $jsonReleaseFile -Name "releases.json" -Force
 }
