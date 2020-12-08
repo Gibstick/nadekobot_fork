acc1dbc1040bf1ef664d8bf039ee7f0d0f40d03f
Cata <ole.kh@hotmail.com>
2020-12-08 20:59:30 +0100

    .buy checks if you already bought a role.

--- NadekoBot.Core/Modules/Gambling/FlowerShopCommands.cs
+++ NadekoBot.Core/Modules/Gambling/FlowerShopCommands.cs
@@ -108,6 +108,12 @@ namespace NadekoBot.Modules.Gambling
                         await ReplyErrorLocalizedAsync("shop_role_not_found").ConfigureAwait(false);
                         return;
                     }
+                    
+                    if (guser.RoleIds.Any(id => id == role.Id))
+                    {
+                        await ReplyErrorLocalizedAsync("shop_role_already_bought").ConfigureAwait(false);
+                        return;
+                    }
 
                     if (await _cs.RemoveAsync(ctx.User.Id, $"Shop purchase - {entry.Type}", entry.Price).ConfigureAwait(false))
                     {
