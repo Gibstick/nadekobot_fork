90536a906f56006014e729b473a3db6adba1ef8c
Kwoth <master.kwoth@gmail.com>
2021-05-18 02:07:39 +0200

    AdjustVolume changed to a safe version

--- NadekoBot.Core/Modules/Music/Common/MusicPlayer.cs
+++ NadekoBot.Core/Modules/Music/Common/MusicPlayer.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using NLog;
 using System.Linq;
+using System.Runtime.InteropServices;
 using NadekoBot.Extensions;
 using NadekoBot.Common.Collections;
 using NadekoBot.Modules.Music.Services;
@@ -587,24 +588,37 @@ namespace NadekoBot.Modules.Music.Common
         }
 
         //aidiakapi ftw
-        public static unsafe byte[] AdjustVolume(byte[] audioSamples, float volume)
+        // public static unsafe byte[] AdjustVolume(byte[] audioSamples, float volume)
+        // {
+        //     if (Math.Abs(volume - 1f) < 0.0001f) return audioSamples;
+        //
+        //     // 16-bit precision for the multiplication
+        //     var volumeFixed = (int)Math.Round(volume * 65536d);
+        //
+        //     var count = audioSamples.Length / 2;
+        //
+        //     fixed (byte* srcBytes = audioSamples)
+        //     {
+        //         var src = (short*)srcBytes;
+        //
+        //         for (var i = count; i != 0; i--, src++)
+        //             *src = (short)(((*src) * volumeFixed) >> 16);
+        //     }
+        //
+        //     return audioSamples;
+        // }
+        
+        private static void AdjustVolume(byte[] audioSamples, float volume)
         {
-            if (Math.Abs(volume - 1f) < 0.0001f) return audioSamples;
+            if (Math.Abs(volume - 1f) < 0.0001f) return;
+            
+            var samples = MemoryMarshal.Cast<byte, short>(audioSamples);
 
-            // 16-bit precision for the multiplication
-            var volumeFixed = (int)Math.Round(volume * 65536d);
-
-            var count = audioSamples.Length / 2;
-
-            fixed (byte* srcBytes = audioSamples)
+            for (var i = 0; i < samples.Length; i++)
             {
-                var src = (short*)srcBytes;
-
-                for (var i = count; i != 0; i--, src++)
-                    *src = (short)(((*src) * volumeFixed) >> 16);
+                ref var sample = ref samples[i];
+                sample = (short) (sample * volume);
             }
-
-            return audioSamples;
         }
 
         public bool ToggleRepeatSong()
