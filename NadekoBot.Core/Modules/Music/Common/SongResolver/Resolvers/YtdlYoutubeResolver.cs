62a546122e7abc379b0bd5f9e9920e7e9e586925
Kwoth <master.kwoth@gmail.com>
2021-06-01 10:55:02 +0200

    - removed tests from pipeline until bot is moved to netcoreapp3.1 because they're not compiling on older sdk - Nicely printing some exceptions related to ffprobe and youtube-dl

--- NadekoBot.Core/Modules/Music/Common/SongResolver/Resolvers/YtdlYoutubeResolver.cs
+++ NadekoBot.Core/Modules/Music/Common/SongResolver/Resolvers/YtdlYoutubeResolver.cs
@@ -99,6 +99,9 @@ namespace NadekoBot.Core.Modules.Music
         
         private YtTrackData ResolveYtdlData(string ytdlOutputString)
         {
+            if (string.IsNullOrWhiteSpace(ytdlOutputString))
+                return default;
+            
             var dataArray = ytdlOutputString.Trim().Split('\n');
 
             if (dataArray.Length < 5)
