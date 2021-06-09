4713b8d864a8a4a262c29c359233290f7b62d47c
Kwoth <master.kwoth@gmail.com>
2021-06-09 17:50:54 +0200

    Added .config xp txt.per_image and xpFromImage to xp.yml - Change this config to allow xp gain from posting images. Images must be 128x128 or greater in size

--- NadekoBot.Core/Modules/Xp/Services/XpService.cs
+++ NadekoBot.Core/Modules/Xp/Services/XpService.cs
@@ -21,6 +21,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using NadekoBot.Core.Modules.Xp;
 using Serilog;
 using StackExchange.Redis;
@@ -593,7 +594,18 @@ namespace NadekoBot.Modules.Xp.Services
                 if (!ShouldTrackXp(user, arg.Channel.Id))
                     return;
 
-                if (!arg.Content.Contains(' ') && arg.Content.Length < 5)
+                var xp = 0;
+                if (arg.Attachments.Any(a => a.Height >= 128 && a.Width >= 128))
+                {
+                    xp = _xpConfig.GetRawData().XpFromImage;
+                }
+                
+                if (arg.Content.Contains(' ') || arg.Content.Length >= 5)
+                {
+                    xp = Math.Max(xp, _xpConfig.GetRawData().XpPerMessage);
+                }
+
+                if (xp <= 0)
                     return;
 
                 if (!SetUserRewarded(user.Id))
@@ -604,7 +616,7 @@ namespace NadekoBot.Modules.Xp.Services
                     Guild = user.Guild,
                     Channel = arg.Channel,
                     User = user,
-                    XpAmount = _xpConfig.Data.XpPerMessage
+                    XpAmount = xp 
                 });
             });
             return Task.CompletedTask;
