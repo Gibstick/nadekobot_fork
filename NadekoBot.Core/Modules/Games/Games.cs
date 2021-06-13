16c7cea914a1efd31a1872dc4f3692be0083b722
Kwoth <master.kwoth@gmail.com>
2021-06-13 03:48:09 +0200

    Fixed compile error

--- NadekoBot.Core/Modules/Games/Games.cs
+++ NadekoBot.Core/Modules/Games/Games.cs
@@ -47,7 +47,7 @@ namespace NadekoBot.Modules.Games
             if (string.IsNullOrWhiteSpace(question))
                 return;
 
-            var res = _service.GetEightballResponse(question);
+            var res = _service.GetEightballResponse(ctx.User.Id, question);
             await ctx.Channel.EmbedAsync(new EmbedBuilder().WithColor(NadekoBot.OkColor)
                 .WithDescription(ctx.User.ToString())
                 .AddField(efb => efb.WithName("❓ " + GetText("question")).WithValue(question).WithIsInline(false))
