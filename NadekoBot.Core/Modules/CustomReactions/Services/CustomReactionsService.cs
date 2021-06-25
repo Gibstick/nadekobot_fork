9ece90f222bd547491249e083b8262ab71140120
Kwoth <master.kwoth@gmail.com>
2021-06-25 19:23:06 +0200

    custom reactions and cleverbot will now show proper permission errors

--- NadekoBot.Core/Modules/CustomReactions/Services/CustomReactionsService.cs
+++ NadekoBot.Core/Modules/CustomReactions/Services/CustomReactionsService.cs
@@ -406,7 +406,7 @@ namespace NadekoBot.Modules.CustomReactions.Services
                     {
                         if (pc.Verbose)
                         {
-                            var returnMsg = _strings.GetText("trigger", sg.Id,
+                            var returnMsg = _strings.GetText("perm_prevent", sg.Id,
                                 index + 1,
                                 Format.Bold(pc.Permissions[index].GetCommand(_cmd.GetPrefix(guild), sg)));
                             try
