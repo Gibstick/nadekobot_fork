d3715aa7a4874903222b43ae7bff7e6655e08efa
Miky <vyasmiky@gmail.com>
2021-09-21 12:38:50 -0400

    added quoteauthor command and sorting by author

--- NadekoBot.Core/Services/Database/Repositories/IQuoteRepository.cs
+++ NadekoBot.Core/Services/Database/Repositories/IQuoteRepository.cs
@@ -10,6 +10,7 @@ namespace NadekoBot.Core.Services.Database.Repositories
         Task<Quote> SearchQuoteKeywordTextAsync(ulong guildId, string keyword, string text);
         IEnumerable<Quote> GetGroup(ulong guildId, int page, OrderType order);
         IEnumerable<Quote> SearchQuoteKeywordKeyTextAsync(ulong guildId, string keyword);
+        IEnumerable<Quote> SearchQuoteAuthorTextAsync(ulong guildId, ulong Authorid,int page);
         void RemoveAllByKeyword(ulong guildId, string keyword);
     }
 }
