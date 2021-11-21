5f87f6b3466d341aa02749faf5368d9b1bd55a66
Miky <vyasmiky@gmail.com>
2021-11-21 16:41:08 -0500

    added sql validation to query only quotes table

--- NadekoBot.Core/Modules/Utility/SqlCommands.cs
+++ NadekoBot.Core/Modules/Utility/SqlCommands.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Discord;
 using NadekoBot.Core.Modules.Utility.Services;
 using System.Linq;
+using System.Collections.Generic;
 using Serilog;
 
 namespace NadekoBot.Modules.Utility
@@ -20,8 +21,17 @@ namespace NadekoBot.Modules.Utility
             {
  
                 try{
-                   var result = _service.SelectSql(sql);
-                   await ctx.SendPaginatedConfirmAsync(0, (cur) =>
+                    // var tablest = _service.tables();
+                    // foreach(var tt in tablest){
+                    //     Log.Information(tt);
+                    // }
+                    var tabresult = _service.SelectSql("SELECT name FROM sqlite_master WHERE type = \"table\"");
+                    var tables = tabresult.Results.Take(100).Select(x => x[0]).ToList();
+                    if (validatesql(sql,tables)== false){
+                        return;
+                    }
+                    var result = _service.SelectSql(sql);
+                    await ctx.SendPaginatedConfirmAsync(0, (cur) =>
                 {
                     var items = result.Results.Skip(cur * 20).Take(20);
 
@@ -43,6 +53,22 @@ namespace NadekoBot.Modules.Utility
                 }catch (Exception e){
                     await ctx.Channel.SendErrorAsync(e.Message);
                 }
+            }
+
+            public Boolean validatesql(string sql,List<string> tables){
+                var sqllower = sql.ToLowerInvariant();
+                var excludetable = true;
+                var includetable=false;
+                foreach(var table in tables){
+                    if ((table == "Quotes") & (sqllower.Contains("quotes"))){
+                        includetable=true;
+
+                    }else if (sqllower.Contains(table.ToLowerInvariant())){
+                        excludetable = false; 
+                    }
+                }
+
+                return (excludetable) & (includetable);
             }
 
         }
